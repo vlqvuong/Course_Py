46d8eafc600d04227ecddedd3db0a56b3e4084ad
vlqvuong <vuong01679536722.com.vn>
2022-02-24 22:56:51 +0700

    py

---
+++ Day_1/Exercise 1.1_Printing, String Manipulation.py
@@ -0,0 +1,26 @@
+print("Hello World!")
+# 5.[Interactive Coding Exercise] Printing
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("Hello World!\nHello World!\nHello World!")
+# Các String sau "\n" sẽ tự động xuống hàng
+print("Hello"+"Vuong")
+# Ghép 2 String lại với nhau bằng dấu "+"
+# Kết quả sẽ gắn liền với nhau: HelloVuong
+print("Hello"+" "+"Vuong")
+print("Hello " + "Vuong")
+print("Hello" + " Vuong")
+# Có 3 cách để thêm dấu cách , đều cho kết quả là: Hello Vuong.
+#7.1 Debugging
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+# Input() 
+print("Hello " + input("What is your name?") + "!")
+# Ctrl + / chuyển dòng đặt con trỏ chuột thành chú thích và ngược lại
+
+
+
+
